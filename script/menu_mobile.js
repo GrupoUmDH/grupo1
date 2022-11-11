5824742ea96aaf4155650c885720f664d8600c15
Lucas Afonso <lucasafonsob@hotmail.com>
2022-11-10 22:15:10 -0300

    Menu Hamburguer

--- script/menu_mobile.js
+++ script/menu_mobile.js
@@ -1,19 +1,16 @@
 window.addEventListener('load', function(){
 
-    let navBarMobile = document.getElementById('navbar-mobile');
     let opMenu = document.getElementById('op-menu');
 
     let atvMenu = document.getElementById('check-menu');
 
     console.log(atvMenu.checked);
 
-
     atvMenu.addEventListener('click', function(){
         if(atvMenu.checked){
             console.log("ativo");
 
             opMenu.style.animation = "deslizar";
-
             opMenu.style.transform = "translateX(0%)";
 
         } else {
