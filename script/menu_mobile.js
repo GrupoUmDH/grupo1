432d3bf55488b7aaf7fb64e95cd5f0feadc8ae48
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-11-14 20:44:13 -0200

    atulaizando hamburguer

--- script/menu_mobile.js
+++ script/menu_mobile.js
@@ -1,8 +1,8 @@
-window.addEventListener('load', function(){
-
     let opMenu = document.getElementById('op-menu');
 
     let atvMenu = document.getElementById('check-menu');
+
+    let navMobile = document.getElementById("navbar-mobile");
 
     console.log(atvMenu.checked);
 
@@ -10,16 +10,15 @@ window.addEventListener('load', function(){
         if(atvMenu.checked){
             console.log("ativo");
 
-            opMenu.style.animation = "deslizar";
+            //opMenu.style.animation = "deslizar";
             opMenu.style.transform = "translateX(0%)";
+            opMenu.style.zIndex = "10";
+            navMobile.style.zIndex = "8";
 
         } else {
             console.log("desativado");
 
             opMenu.style.transform = "translateX(-100%)";
+            opMenu.style.zIndex = "-1";
         }
     });
-
-
-
-});
