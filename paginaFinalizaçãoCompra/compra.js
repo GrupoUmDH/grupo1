e20331c9bbd9272c614336a06420481deb9f3b27
vagnerb79 <spotify.vagner@gmail.com>
2022-11-17 18:55:37 -0300

    Criar Validações

--- paginaFinalizaçãoCompra/compra.js
+++ paginaFinalizaçãoCompra/compra.js
@@ -1,66 +1,36 @@
-window.addEventListener('load', function(){
 
-    //VALIDAR CAMPOS OBRIGATÓRIOS
-const form = document.querySelector('main .content-1.content-form');
 
-form.addEventListener('submit', function(event){
-    //VERIFICAR CAMPOS EM BRANCO
-    event.preventDefault();
-    const inputList = document.querySelectorAll('main content-1.conten-form input');
+   
+const form = document.querySelector('main .content-form form')
+const inputList = document.querySelectorAll(`main .content-1 input:not(.check)`)
+const cep = document.querySelector("input[name='codigo-postal']")
+const endereco = document.querySelector("input[name='endereco']")
+const cidade = document.querySelector("input[name='cidade']")
+const estado = document.querySelector("input[name='estado']")
+const pais = document.querySelector("input[name='pais']")
+
+form.addEventListener('submit', function(event){   
+
     inputList.forEach(input => {
         if (!input.value){
-            const errorSpan = document.createElement ('span');
-            errorSpan.classList.add('error');
-            errorSpan.innerText = 'Campo obrigatório'
-            input.insertAdjacentElement('afterend' , errorSpan);
+            event.preventDefault();
+            document.querySelector('.err-'+ input.name).classList.remove('invisible')
+        } else{
+            document.querySelector('.err-'+ input.name).classList.add('invisible')
         }
     })
 });
-});
 
+cep.addEventListener('change', () => {
+    if(cep.value.length != 8) return
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+        .then(res => res.json())
+        .then(data => {
+            endereco.value = data.logradouro
+            cidade.value = data.uf
+            estado.value = data.localidade
+            pais.value = 'Brasil'
+        })
 
+})
 
-
-
-
-
-
-
-// window.addEventListener('load', function () {
-//     let form = document.querySelector('main form.form-card');
-//     let inputList = document.querySelectorAll('main form.form-card input');
-
-//     const removeErros = function () {
-//         const errorSpan = document.querySelectorAll('main form.form-card span.error');
-//         errorSpan.forEach(span => span.remove());
-//     }
-
-//     const createError = function (input, mensagem) {
-//         const errorSpan = document.createElement('span');
-//         errorSpan.classList.add('error');
-//         errorSpan.innerText = mensagem;
-
-//         input.insertAdjacentElement('afterend', errorSpan);
-//     }
-
-//     form.addEventListener('submit', function (event) {
-
-//         let hasError = false;
-
-//         event.preventDefault();
-//         console.log("testando...");
-//         removeErros();
-
-//         inputList.forEach(input => {
-//             if (!input.value) {
-//                 hasError = true;
-//                 createError(input, "Campo Obrigatório!")
-//             }
-//         });
-
-//         if (!hasError) {
-//             this.submit();
-//         }
-
-//     });
-// });
