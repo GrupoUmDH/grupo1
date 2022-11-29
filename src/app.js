9ee807d80e770a9c7b917054864ebf31d77f510d
lucasafonsobastos <lucasafonsob@hotmail.com>
2022-11-29 20:23:45 -0300

    atualizando

--- src/app.js
+++ src/app.js
@@ -4,15 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var homeRouter = require('./views/home');
+var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.use(express.static(path.join(__dirname, '../public')));
-
-app.set('views','./views');
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -25,20 +23,22 @@ app.use(cookieParser());
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
 
+app.use(express.static(path.join(__dirname, '../public')));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
