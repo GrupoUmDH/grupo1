7c255e407a92ede3fcad5103dec6cf8c80d43745
DaviSiqueiraS <davisiqueiras@hotmail.com>
2022-12-04 17:53:36 -0300

    alteracoes series ejs

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -6,3 +6,10 @@ module.exports = {
         return res.render('filmes', { pageName: 'Filmes' })
     }
 }
+
+module.exports = {
+    series: (req, res) =>{
+        const series = ProdutosModel.series();
+        return res.render('series', { pageName: 'Series' })
+    }
+}
