542fb777b012e1ba442f9f2d8c6456da07a6f6ee
Anna Paula Pedrussi <annapaulapedrussi@gmail.com>
2022-12-10 11:44:48 -0300

    juntando branches

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -4,26 +4,19 @@ var router = express.Router();
 const ProdutosController = require('../controllers/ProdutosController')
 
 
-// router.get('/series', ProdutoController.series);
-
 router.get('/', function(req, res, next) {
     res.render('produtos', { pageName: 'produtos', js:'adicionarAoCarrinho' });
 });
 
 router.get('/filmes', ProdutosController.filmes);
 
-// router.get('/series', ProdutosController.series);
-    
-
-router.get('/series', function(req, res, next) {
-    res.render('series', { pageName: 'series' });
-});
+router.get('/series', ProdutosController.series);
 
 router.get('/cadastroProduto', function(req, res, next) {
     res.render('cadastroProduto', { pageName: 'cadastroProduto' });
-
 });
-// router.get('/categorias', ProdutosController.listar);
+
+router.get('/categorias', ProdutosController.listar);
 
 
 module.exports = router;
