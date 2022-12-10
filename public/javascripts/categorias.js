4ad4d3887389655dccaf12750c9859b21ed72657
Anna Paula Pedrussi <annapaulapedrussi@gmail.com>
2022-12-09 21:05:03 -0300

    ajuste js

--- public/javascripts/categorias.js
+++ public/javascripts/categorias.js
@@ -1,7 +1,8 @@
 const produto= document.querySelectorAll("section section .filme-show")[1];
+const filmes = [].slice.call(produto);
 
-produto.forEach(element=>{
-    addEventListener('click', function(event){
+filmes.forEach(function(e){
+    e.addEventListener('click', function(event){
         console.log('teste')
     })
 });
