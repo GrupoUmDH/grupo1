b5c7592f1c3c5311bf1cbcc1292377e2054aa63b
Anna Paula Pedrussi <annapaulapedrussi@gmail.com>
2022-12-12 15:25:21 -0300

    merge

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -20,17 +20,9 @@ module.exports = {
         const produtos = ProdutosModel.listar(categoria);
         return res.render('categorias', { pageName: 'categorias', produtos, categoria, js: 'categorias' });
     },
-<<<<<<< HEAD
-    produtos: (req, res) => {
-        const filmes = ProdutosModel.produtos();
-        return res.render('produtos', { pageName: 'produtos', Filme: filmes, js: 'adicionarAoCarrinho' });
-    }
-   
-=======
     produto: (req, res) => {
         const filme = filmeModel.index();
         return res.render('produtos', { pageName: 'produtos',Filme: filme , js: 'adicionarAoCarrinho' });
     } 
->>>>>>> lucas
 }
 
