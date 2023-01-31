98f456d02a64de84a38026b2b0a652d929c9f451
vagnerb79 <spotify.vagner@gmail.com>
2023-01-31 11:41:42 -0300

    Ajustando CRUD e Middlewares

--- src/middlewares/validadorForm.js
+++ src/middlewares/validadorForm.js
@@ -1,5 +1,19 @@
 const { check } = require('express-validator');
 
 module.exports = [
-    check('nome').notEmpty()
+    // check('id')
+    // .notEmpty().withMessage('ID obrigatório').bail(),
+
+    check('nome')
+    .notEmpty().withMessage('Nome obrigatório').bail(),
+    
+    check('descricao')
+    .notEmpty().withMessage('Descrição obrigatória').bail(),
+    
+    check('classificacao')
+    .notEmpty().withMessage('Classificação obrigatória').bail(),
+
+    check('valor')
+    .notEmpty().withMessage('Valor obrigatório').bail()
+        
 ]
