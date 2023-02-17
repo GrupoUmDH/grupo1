001a2d8dba393c2c674c70a73e2d73c05a80e081
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-02-16 21:42:48 -0300

    att

--- controllers/FilmesControllers.js
+++ controllers/FilmesControllers.js
@@ -1,9 +1,23 @@
 const { Categorias } = require('../models');
+const { Classificacao } = require('../models');
+const { Filme } = require("../models");
+
 
 module.exports = {
     index: async (req, res) => {
         const categorias = await Categorias.findAll();
-        res.render('teste',{categorias});
+        const classificacoes = await Classificacao.findAll();
+        const filmes = await Filme.findAll(
+            {
+                include: [{
+                    model: Classificacao,
+                    as: 'indicacao'
+                }]
+            }
+        );
+        console.log(filmes);
+
+        res.render('teste',{categorias, classificacoes, filmes});
         
     }
 }
