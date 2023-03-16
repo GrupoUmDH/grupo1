5d8a5cf95e5ae2a01f981455479cae8041a24d2d
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-03-16 15:29:31 -0300

    Atualizando branch

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,18 +4,12 @@ const router = express.Router();
 const userController = require('../../controllers/UserControllers');
 const loginSession = require('../middlewares/loginSession')
 
-/* GET users listing. */
-router.get('/', loginSession, userController.index);
 
-//router.get('/criar',userController.criarForm);
+router.get('/', loginSession, userController.index);
 
 router.post('/user', userController.login);
 
 router.post("/cadastro", userController.cadastro);
-//router.post('/login', userController.login);
-
-//router.post('/novo', userController.novoUsuario);
-
 
 
 
