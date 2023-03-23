520904399902371ba9f61d1e830fa4545760a6bd
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-03-23 15:43:23 -0300

    Função de pesquisa pela API

--- src/request/search.js
+++ src/request/search.js
@@ -4,12 +4,31 @@ const api = require('../../config/api.json')
 const axios = require('axios');
 
 const search = {
-    getFilme: (filme) =>
-        axios({
+    // getFilme - recebe como parametro a query de busca na API 
+    getFilme: (filme) => {
+        return axios({
             method: "get",
             url: `${api.url}/search/movie?${api.key}&${api.language}&query=${filme}`,
             timeout: 4000,
-        }),
+        })
+    },
+
+    // getPopular - esta requesição busca os filmes populares
+    getTopFilmes: () =>{
+        return axios({
+            method: "get",
+            url: `${api.url}/movie/popular?${api.key}&${api.language}&page=1`,
+            timeout: 4000,
+        });
+    },
+
+    getTopSeries: () =>{
+        return axios({
+            method: "get",
+            url: `${api.url}/tv/top_rated?${api.key}&${api.language}&page=1`,
+            timeout: 4000,
+        });
+    },
 };
 
 module.exports = search;
