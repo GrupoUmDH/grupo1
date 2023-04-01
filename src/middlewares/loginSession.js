9fe022469c09df53c7178f66ae3c56c75bce928d
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-03-31 23:01:53 -0300

    atualizando

--- src/middlewares/loginSession.js
+++ src/middlewares/loginSession.js
@@ -1,20 +1,9 @@
 module.exports = [
     login = (req, res, next) => {
 
-        const usuario = {nome:req.session.name, email:req.session.email}
-
-
-         console.log(req.session.email,req.session.tipo)
+        console.log(req.session.email,req.session.tipo)
         if (req.session.email) {
-            if(req.session.tipo == 'admin'){
-                res.redirect("/painel");
-                
-            }else {
-                next();
-                //console.log("Aqui");
-                //res.redirect('/painel/painel-user');
-                //res.render('painel-user', { pageName: 'painel-user', js: '', usuario });
-            }
+            res.redirect("/painel");
         } else {
             next();
         }
